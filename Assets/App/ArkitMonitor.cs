175b7a50cba5fd107bae5593f64be00e5cfe60a2
Graham Reeves <graham@grahamreeves.com>
2018-03-29 18:33:38 +0100

    Seperate monitor handling state, pointcloud & camera for stream

---
+++ Assets/App/ArkitMonitor.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_IOS
+using UnityEngine.XR.iOS;
+#endif
+
+
+
+[System.Serializable]
+public class UnityEvent_Vector3Array : UnityEngine.Events.UnityEvent<Vector3[]> { }
+
+
+[System.Serializable]
+public class UnityEvent_PositionRotation : UnityEngine.Events.UnityEvent<Vector3, Quaternion> { }
+
+
+public class ArkitMonitor : MonoBehaviour {
+
+	public UnityEvent_String			OnStatusUpdate;
+	public UnityEvent_Vector3Array		OnCloudChanged;
+	public UnityEvent_PositionRotation	OnCameraTransformUpdate;
+
+	Vector3[] LastCloud;
+
+
+	void OnEnable()
+	{
+		UnityARSessionNativeInterface.ARFrameUpdatedEvent += OnUpdate;
+		/*
+		Session = UnityARSessionNativeInterface.GetARSessionNativeInterface();
+
+		Application.targetFrameRate = 60;
+		var config = new ARKitWorldTrackingSessionConfiguration();
+		config.planeDetection = planeDetection;
+		config.alignment = startAlignment;
+		config.getPointCloudData = getPointCloud;
+		config.enableLightEstimation = enableLightEstimation;
+
+		if (!config.IsSupported)
+		{
+			OnStatusUpdate.Invoke("Config not supported");
+			throw new System.Exception("Config not supported");
+		}
+
+		Session.RunWithConfig(config);
+		*/
+	}
+
+	void OnDisable()
+	{
+		UnityARSessionNativeInterface.ARFrameUpdatedEvent += OnUpdate;
+	}
+
+
+	//	returns if changed
+	bool UpdatePointCloud(Vector3[] NewCloud)
+	{
+		var NewLength = (NewCloud != null) ? NewCloud.Length : 0;
+		var LastLength = (LastCloud != null) ? LastCloud.Length : 0;
+		if ( NewLength != LastLength )
+		{
+			LastCloud = NewCloud;
+			return true;
+		}
+
+		//	probably null
+		if ( NewLength == 0 )
+		{
+			LastCloud = NewCloud;
+			return true;
+		}
+
+		//	check diff
+		var MinChange = float.Epsilon;
+		var Same = true;
+		for (var i = 0; i < LastCloud.Length;	i++ )
+		{
+			var LastPos = LastCloud[i];
+			var NewPos = NewCloud[i];
+			var Change = Vector3.Distance(NewPos, LastPos);
+			if (Change <= MinChange)
+				continue;
+			Same = false;
+			break;
+		}
+
+		LastCloud = NewCloud;
+		return Same;
+	}
+
+	void OnUpdate(UnityARCamera cam)
+	{
+		var NewCloudData = cam.pointCloudData;
+		var CloudChanged = UpdatePointCloud(NewCloudData);
+		if ( CloudChanged )
+		{
+			OnCloudChanged.Invoke(LastCloud);
+		}
+
+		//	report state
+		var State = "";
+		var ReasonString = cam.trackingReason.ToString().Substring("ARTrackingStateReason".Length);
+		switch ( cam.trackingState )
+		{
+			case ARTrackingState.ARTrackingStateNormal:
+				State = "Tracking";
+				break;
+
+			case ARTrackingState.ARTrackingStateNotAvailable:
+				State = "Not availible (" + ReasonString + ")";
+				break;
+
+			case ARTrackingState.ARTrackingStateLimited:
+				State = "Limited tracking (" + ReasonString + ")";
+				break;
+		}
+		OnStatusUpdate.Invoke(State);
+
+		if (cam.trackingState != ARTrackingState.ARTrackingStateNormal)
+			return;
+
+		//	get new pose
+		var WorldMatrixArkit = cam.worldTransform;
+		var WorldMatrix = new Matrix4x4();
+		WorldMatrix.SetColumn(0, WorldMatrixArkit.column0);
+		WorldMatrix.SetColumn(1, WorldMatrixArkit.column1);
+		WorldMatrix.SetColumn(2, WorldMatrixArkit.column2);
+		WorldMatrix.SetColumn(3, WorldMatrixArkit.column3);
+
+		var WorldPosition = UnityARMatrixOps.GetPosition(WorldMatrix);
+		var WorldRotation = UnityARMatrixOps.GetRotation(WorldMatrix);
+
+		OnCameraTransformUpdate.Invoke( WorldPosition, WorldRotation );
+	}
+}
--- Assets/App/ArkitStreamer.cs
+++ Assets/App/ArkitStreamer.cs
@@ -15,6 +15,23 @@ public class ArkitCameraTransform
 		this.Position = transform.localPosition;
 		this.Rotation = transform.localRotation;
 	}
+
+	public ArkitCameraTransform(Vector3 Position,Quaternion Rotation)
+	{
+		this.Position = Position;
+		this.Rotation = Rotation;
+	}
+}
+
+[System.Serializable]
+public class ArkitPointCloud
+{
+	public Vector3[] Positions;
+
+	public ArkitPointCloud(Vector3[] Positions)
+	{
+		this.Positions = Positions;
+	}
 }
 
 
@@ -25,46 +42,76 @@ public class ArkitCameraTransformPacket : BasePacket
 
 	public static string EncodingName { get { return typeof(ArkitCameraTransform).ToString(); } }
 
-	public ArkitCameraTransformPacket(Transform transform, string StreamName)
+	public ArkitCameraTransformPacket(Vector3 Position,Quaternion Rotation, string StreamName)
 	{
 		//	this shows that PopRelayClient and PopRelayDecoder probbaly need Encoding() funcs
 		//	encoding is json, but OUR decoder figures that out
 		//	Data is also unused, we just re-decode the original json
 		this.Timecode = PopX.Time.GetTodayUtcTimeMs();
-		this.Encoding = ArkitCameraTransformPacket.EncodingName;
+		this.Encoding = EncodingName;
 		this.Data = null;
 		this.Stream = StreamName;
-		this.Transform = new ArkitCameraTransform(transform);
+		this.Transform = new ArkitCameraTransform(Position,Rotation);
 	}
 
 }
 
 
+[System.Serializable]
+public class ArkitPointCloudPacket : BasePacket
+{
+	public ArkitPointCloud PointCloud;
+
+	public static string EncodingName { get { return typeof(ArkitPointCloud).ToString(); } }
+
+	public ArkitPointCloudPacket(Vector3[] PointCloud,string StreamName)
+	{
+		//	this shows that PopRelayClient and PopRelayDecoder probbaly need Encoding() funcs
+		//	encoding is json, but OUR decoder figures that out
+		//	Data is also unused, we just re-decode the original json
+		this.Timecode = PopX.Time.GetTodayUtcTimeMs();
+		this.Encoding = EncodingName;
+		this.Data = null;
+		this.Stream = StreamName;
+		this.PointCloud = new ArkitPointCloud(PointCloud);
+	}
+
+}
+
 public class ArkitStreamer : MonoBehaviour {
 
 	PopRelayClient				Client { get { return GameObject.FindObjectOfType<PopRelayClient>(); }}
-	public RepeatActionTimer	Timer;
-	public Transform			ArkitCameraTransform;
-	public string				StreamName = "ArkitCameraTransform";
+
+	public ArkitMonitor			Monitor	{ get { return GameObject.FindObjectOfType<ArkitMonitor>(); } }
+
+	public string				PointCloudStreamName = "ArkitPointCloud";
+	public string				CameraTransformStreamName = "ArkitCameraTransform";
 
 	void OnEnable()
 	{
-		if (ArkitCameraTransform == null)
-			ArkitCameraTransform = Camera.main.transform;
-		if (string.IsNullOrEmpty(StreamName))
-			StreamName = this.name;
+		Monitor.OnCloudChanged.AddListener(OnCloudChanged);
+		Monitor.OnCameraTransformUpdate.AddListener(OnTransformChanged);
 	}
 
-	void SendCameraPosition()
+	void OnDisable()
 	{
-		var Packet = new ArkitCameraTransformPacket(ArkitCameraTransform, StreamName);
-		if ( Client.IsConnected)
-			Client.SendJson( Packet );
+		Monitor.OnCloudChanged.RemoveListener(OnCloudChanged);
+		Monitor.OnCameraTransformUpdate.RemoveListener(OnTransformChanged);
 	}
 
-
-	void Update()
+	void OnCloudChanged(Vector3[] Points)
 	{
-		Timer.Update(SendCameraPosition);
+		var Packet = new ArkitPointCloudPacket(Points, PointCloudStreamName);
+		if (Client.IsConnected)
+			Client.SendJson(Packet);
 	}
+
+	void OnTransformChanged(Vector3 Positon,Quaternion Rotation)
+	{
+		var Packet = new ArkitCameraTransformPacket(Positon, Rotation, CameraTransformStreamName);
+		if (Client.IsConnected)
+			Client.SendJson(Packet);
+	}
+
+
 }
